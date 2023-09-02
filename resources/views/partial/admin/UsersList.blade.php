6bc7cd666c4276a24c018b0d1302d2215806dd5b
مهسا دلفانی <M.Delfani@farsif02.local>
2023-09-02 15:48:37 +0330

    test

--- resources/views/partial/admin/UsersList.blade.php
+++ resources/views/partial/admin/UsersList.blade.php
@@ -1,36 +1,9 @@
 @extends('layouts.NewPanel.NewPanel')
 @section('content')
 
-
-
-
-<!-- {{--alert start--}}
-<div class="modal fade" id="deletebox" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">حذف</h1>
-        <!-- {{-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>--}} -->
-<!-- </div>
-<div class="modal-body">
-  از حذف آیتم مطمئن هستید؟
-  <div id="info"></div>
-</div>
-<div class="modal-footer">
-  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">خیر</button>
-  <button type="button" class="btn btn-primary" id="confirm" data-bs-dismiss="modal">حذف شود</button>
-</div>
-</div>
-</div>
-</div> -->
-<!-- {{--alert end--}} -->
-
-
-
-
 <div class="col-12">
   <div class=" mb-5">
-    <div class="col-12 pb-2 px-1 color-s" id="header-t">
+    <div class="col-12 pb-2 px-1 color-s2" id="header-t">
       <h4 class="">لیست کاربران</h4>
     </div>
   </div>
@@ -44,7 +17,7 @@
       <h6 class=" color-s py-3">جستجو</h6>
       <div class="row">
         <div class="col-5 d-flex justify-content-center">
-          <select class="form-select text-secondary color-v slc1 pe-5" aria-label="Default select example">
+          <select class="form-select text-secondary color-v2 slc16 pe-5" aria-label="Default select example">
             <option selected disabled>جستجو مشابه فیلد</option>
             <option value="1">نام مشتری</option>
             <option value="2">کد مشتری</option>
@@ -52,10 +25,10 @@
           </select>
         </div>
         <div class="col-5 px-1 d-flex justify-content-center">
-          <input type="text" class="col-6 form-control color-v slc1 pe-2" placeholder="کلمه کلیدی">
+          <input type="text" class="col-6 form-control color-v2 slc1 pe-2" placeholder="کلمه کلیدی">
         </div>
         <div class="col-2">
-          <button type="submit" class="btn btn-secondary color-s text-white slc1 col-12" id="sub">ثبت</button>
+          <button type="submit" class="btn btn-secondary color-s2 text-white slc16 col-12" id="sub">ثبت</button>
         </div>
       </div>
     </div>
@@ -67,17 +40,17 @@
       <h6 class="color-s py-3">فرم تعریف دسته بندی جدید</h6>
       <div class=" row">
         <div class="col-5 d-flex justify-content-center">
-          <input type="text" class="col-6 form-control color-v slc1 pe-2" placeholder="نام دسته بندی">
+          <input type="text" class="col-6 form-control color-v2 slc12 pe-2" placeholder="نام دسته بندی">
         </div>
         <div class="col-5 d-flex justify-content-center px-1">
-          <select class="form-select text-secondary color-v slc1 pe-5" aria-label="Default select example">
+          <select class="form-select text-secondary color-v2 slc16 pe-5" aria-label="Default select example">
             <option selected disabled>نام و نام خانوادگی</option>
             <option value="1">شماره تماس </option>
             <option value="2">نقش کاربری</option>
           </select>
         </div>
-        <div class="col-2">
-          <button type="submit" class="btn btn-secondary text-white color-s slc1 col-12" id="sub">ثبت</button>
+        <div class="col-2 bg-danger">
+          <button type="submit" class="btn btn-secondary text-white color-s2 slc16 col-12 h-75" id="sub_3">ثبت</button>
         </div>
       </div>
 
@@ -91,17 +64,12 @@
     </div>
   </div>
 </div>
-<!-- 
-<form id="form" method="get" action="{{route('Newusers_permission_List')}}">
-  <input type="hidden" name="id_user" id="id_user" value="">
-</form> -->
-
-
 <!-- table******************************************* -->
-<div class="row align-content-center p-0 m-0">
-  <table id="usersTable" class="table m-0 p-0">
+<div class="py-5">
+  <table class="table align-items-center" id="table-m-3">
     <thead>
       <tr>
+        <th scope="col">ردیف</th>
         <th scope="col">نام</th>
         <th scope="col">نام خانوادگی</th>
         <th scope="col">ایمیل</th>
@@ -112,184 +80,84 @@
       </tr>
     </thead>
     <tbody>
+      <tr>
+        <td>1</td>
+        <td>رنجبری</td>
+        <td>ranjbari_davoud@yahoo.com</td>
+        <td>غیرفعال</td>
+        <td>0919 291 2686</td>
+        <td>......</td>
+        <td>......</td>
+        <td class="icon_box">
+          <i class="px-2 bi bi-pencil-square"></i>
+          <i class="px-2 bi bi-x-circle"></i>
+          <i class="px-2 h4 bi bi-person-plus-fill"></i>
+        </td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>عباسی</td>
+        <td>ali.abbasi1023@gmail.com</td>
+        <td>web developer</td>
+        <td>09364916247</td>
+        <td>......</td>
+        <td>......</td>
+        <td class="icon_box">
+          <i class="px-2 bi bi-pencil-square"></i>
+          <i class="px-2 bi bi-x-circle"></i>
+          <i class="px-2 h4  bi bi-person-plus-fill"></i>
+        </td>
+
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>میرزاجانی</td>
+        <td>na30m9170@gmail.com</td>
+        <td>غیر فعال</td>
+        <td>09392087671</td>
+        <td>......</td>
+        <td>......</td>
+        <td class="icon_box">
+          <i class="px-2 bi bi-pencil-square"></i>
+          <i class="px-2 bi bi-x-circle"></i>
+          <i class="px-2 h4 bi bi-person-plus-fill"></i>
+        </td>
+      </tr>
     </tbody>
   </table>
+
 </div>
-<!-- {{--pagination start--}} -->
-<!-- <nav id="pg1" aria-label="Page navigation example" class="d-flex justify-content-center col-12 p-0 m-0 my-5">
-  <ul id="ppp" class="pagination p-0 m-0 d-flex align-items-center">
-  </ul>
-</nav> -->
-<!-- {{--pagination end--}} -->
 
 
 
 
-<!-- <script>
-  axios({
-      method: "get",
-      url: '{{route('
-      usersList ')}}',
-      headers: {
-        "Content-Type": "multipart/form-data"
-      },
-      timeout: 10000, // 10 seconds timeout
-
-    })
-    .then(function(response) {
-      let table = document.querySelector('#usersTable>tbody');
-      let list = response.data.data;
-      makePaginate(response.data.meta)
-      // console.log(response)
-      list.map(function(val) {
-        table.innerHTML += `
-            <tr data-id='${val.id}'>
-                <td>${val.fname}</td>
-                <td>${val.lname}</td>
-                <td>${val.email}</td>
-                <td>غیرفعال</td>
-                <td>${val.phone}</td>
-                <td>......</td>
-                <td class="icon_box d-flex justify-content-center align-items-center">
-                    <i class="bi bi-pencil-square" title=' ویرایش کاربر'></i>
-                    <i class="mx-3 bi bi-trash3-fill" title='حذف کاربر' onclick="_delete_item(this)" data-bs-toggle="modal" data-bs-target="#deletebox"></i>
-                    <i class="mt-2 h5 bi bi-person-fill-gear" title='دسترسی های کاربر' onclick="GoToper(${val.id})"></i>
-                </td>
-            </tr>
-                    `
-
-      })
-    })
-    .catch(function(response) {
-      //handle error
-      // console.log(response.data.data);
-    })
-
-  // ***************************************** making pagination number
-  function makePaginate(val) {
-    let num = val.last_page;
-    let current_page = val.current_page;
-    let paginate = document.getElementById('ppp');
-    for (p = 1; p <= num; p++) {
-      let li = `
-                    <li class="page-item"><a class="page-link"  onclick="_pagination(this)">${p}</a></li>
-                    `;
-      paginate.innerHTML += li;
-
-    }
-    //     *************color
-    let p_children = paginate.children
-    // console.log(p_children)
-    for (let p = 0; p < p_children.length; p++) {
-      let item = p_children[p].children[0]
-      let text = item.innerHTML
-      if (text == current_page) {
-        item.style.color = 'red'
-        item.style.border = '2px solid red'
-
-      }
-    }
-  }
 
 
-  // **************************************   pagination onclick
-  function _pagination(s) {
-    let num = s.innerText;
-    let url = "http://172.21.5.25/api/Users/NewUsers/List?page=";
-    let urlmix = url + num;
-    console.log(urlmix)
 
-    axios({
-        method: "get",
-        url: urlmix,
-        headers: {
-          "Content-Type": "multipart/form-data"
-        },
-        timeout: 10000, // 10 seconds timeout
 
-      })
 
-      .then(function(response) {
-        let table = document.querySelector('#usersTable>tbody');
-        let list = response.data.data;
 
-        /* خالی کردن تگ ها*/
-        let paginate = document.getElementById('ppp');
-        paginate.innerHTML = '';
-        makePaginate(response.data.meta)
-        table.innerHTML = ''
-        // console.log(response)
-        list.map(function(val) {
-          table.innerHTML += `
-            <tr data-id='${val.id}'>
-                <td>${val.fname}</td>
-                <td>${val.lname}</td>
-                <td>${val.email}</td>
-                <td>غیرفعال</td>
-                <td>${val.phone}</td>
-                <td>......</td>
-                <td class="icon_box d-flex justify-content-center align-items-center">
-                    <i class="bi bi-pencil-square" title=' ویرایش کاربر'></i>
-                    <i class="mx-3 bi bi-trash3-fill" title='حذف کاربر' onclick="_delete_item(this)" data-bs-toggle="modal" data-bs-target="#deletebox"></i>
-                    <i class="mt-2 h5 bi bi-person-fill-gear" title='دسترسی های کاربر' onclick="GoToper(${val.id})" ></i>
-                </td>
-            </tr>
-                    `
 
-        })
-      })
 
-  }
-  // ************************************** confirm and delete item
-  let yes = document.getElementById('confirm');
 
-  function _delete_item(s) {
-    // alert('ok')
-    yes.onclick = function() {
 
-      let _id_tr = s.parentElement.parentElement.getAttribute('data-id')
-      const array = {
-        'id_remove': _id_tr,
-      }
-      axios({
-          method: "post",
-          url: '{{route('
-          NewUsers_delete ')}}',
-          data: array,
-          headers: {
-            "Content-Type": "multipart/form-data"
-          },
-          timeout: 10000, // 10 seconds timeout
 
-        })
-        .then(function(response) {
-          console.log(response)
-          let _children = s.parentElement.parentElement.children
-          for (let c = 0; c < _children.length; c++) {
 
-            _children[c].style.padding = '0px'
-            _children[c].style.height = '0px'
-            _children[c].style.margin = '0px'
-            _children[c].style.fontSize = '0px'
-            _children[c].style.border = 'none'
-          }
-          // console.log(_children)
-          s.parentElement.parentElement.style.height = '0px'
-          s.parentElement.remove()
-        })
-        .catch(function(response) {
-          //handle error
-          // console.log(response);
-        });
-    }
-  }
-</script>
 
-<script>
-  function GoToper(id) {
-    let input = document.getElementById('id_user').value = id;
-    document.getElementById('form').submit();
-  }
-</script> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @endsection
