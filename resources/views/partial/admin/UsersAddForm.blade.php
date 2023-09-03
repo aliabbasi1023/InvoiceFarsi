01297d06cd3292af7270d4f4b46914b7f405ef1f
مهسا دلفانی <M.Delfani@farsif02.local>
2023-09-03 11:17:02 +0330

    form2

--- resources/views/partial/admin/UsersAddForm.blade.php
+++ resources/views/partial/admin/UsersAddForm.blade.php
@@ -11,9 +11,15 @@
 
     <div class="col-12 d-flex py-2">
       <form method="POST" action="{{ route('register') }}" class="col-12 row">
+        @csrf
         <div class=" col-6 p-1 ">
           <label for="name" class="col-md-4 col-form-label text-md-end text-dark">{{ __("نام و نام خانوادگی") }}</label>
-          <input type=" text" class="form-control input_1 px-2" placeholder="نام و نام خانوادگی" id="name">
+          <input type=" text" class="form-control input_1 px-2" name="name" value="{{ old('name') }}" required autocomplete="name" placeholder="نام و نام خانوادگی" id="name">
+          @error('name')
+          <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+          </span>
+          @enderror
         </div>
 
         <div class="col-6 p-1">
@@ -25,13 +31,18 @@
     <div class="col-12 d-flex py-2" id="bdr">
 
       <div class="col-6 p-1 ">
-        <label for="email" class="col-md-4 col-form-label text-md-end text-dark">{{ __('آدرس ایمیل') }}</label>
-        <input type="text" class="form-control input_1 px-2" placeholder="آدرس ایمیل" id="user_name">
+        <label for="email" class="col-md-4 col-form-label text-md-end text-dark">ایمیل (نام کاربری)</label>
+        <input type="text" class="form-control input_1 px-2" placeholder="آدرس ایمیل" id="user_name" name="email" value="{{ old('email') }}" required autocomplete="email">
+        @error('email')
+        <span class="invalid-feedback" role="alert">
+          <strong>{{ $message }}</strong>
+        </span>
+        @enderror
       </div>
 
       <div class="col-6 p-1">
-        <label for="password" class="col-md-4 col-form-label text-md-end text-dark">{{ __("رمز عبور") }}</label>
-        <input type="text" class="form-control input_1 px-2" placeholder="رمز عبور" id="password">
+        <label for="password" class="col-md-4 col-form-label text-md-end text-dark">کلمه عبور</label>
+        <input type="text" class="form-control input_1 px-2" placeholder="رمز عبور" id="password" name="password" required autocomplete="new-password">
       </div>
 
     </div>
@@ -39,7 +50,7 @@
 
       <div class="col-6 p-1 ">
         <label for="password-confirm" class="col-md-4 col-form-label text-md-end text-dark">{{ __('تکرار رمز عبور') }}</label>
-        <input type="text" class="form-control input_1 px-2" placeholder="تکرار رمز عبور" id=" repeat_pass">
+        <input type="text" class="form-control input_1 px-2" placeholder="تکرار رمز عبور" id=" repeat_pass" name="password_confirmation" required autocomplete="new-password" لهف>
       </div>
 
       <div class="col-6 p-1">
